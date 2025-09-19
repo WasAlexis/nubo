ab260413370eca04791a838f2acc4c98583c53a6
Alexis <alexisnomain@icloud.com>
2025-09-18 20:47:55 -0400

    chore: install pg and cors dependencies

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to Nubo" });
-})
+});
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${port}`);
