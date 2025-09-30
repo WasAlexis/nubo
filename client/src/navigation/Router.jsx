d94190bccf90769a7207b05cbd265373c008f9fa
Alexis <alexisnomain@icloud.com>
2025-09-29 21:54:58 -0400

    feat: assign a route in navigation

--- client/src/navigation/Router.jsx
+++ client/src/navigation/Router.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login.jsx';
 
 function Router() {
     return (
         <Routes>
             <Route path="/" element={ <h1>Welcome to Nubo</h1> } />
+            <Route path='/login' element={ <Login /> } />
         </Routes>
     );
 }
